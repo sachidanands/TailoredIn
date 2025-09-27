7e68841d97f9f8c5a86b87338c51fa0e902fa446
Sachidanand Singh <sachidanands18@gmail.com>
2025-09-27 08:03:12 +0000

    hey can you check once again if you have placed the google tag manager i

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -233,15 +233,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth" suppressHydrationWarning>
       <head>
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-M4V26FT4');
-          `}
-        </Script>
         <link rel="canonical" href="https://www.tailoredin.online" />
         <meta name="google-site-verification" content="5M8a6xWs3w2DKEKVlZLlC2Wh3tcWa9iOxgo1Qb80m94" />
         <meta charSet="UTF-8" />
