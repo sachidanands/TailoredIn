bdc465bbfa22785c4883e882c8a38a04ff180ba7
Sachidanand Singh <sachidanands18@gmail.com>
2025-09-27 10:13:13 +0000

    schema correction

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -243,14 +243,6 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-        />
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(faqSchema) }}
-        />
       </head>
       <body className="font-body antialiased">
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M4V26FT4"
