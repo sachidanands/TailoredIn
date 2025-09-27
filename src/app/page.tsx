60c911acd572d9b1a1d406bbe067ca5b70cc795d
Sachidanand Singh <sachidanands18@gmail.com>
2025-09-27 07:56:30 +0000

    build is failing with this erros
     ⨯ useSearchParams() should be wrapped

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import { Header } from '@/components/landing/header';
 import { HeroSection } from '@/components/landing/hero-section';
 import { AboutSection } from '@/components/landing/about-section';
@@ -11,7 +12,9 @@ import { GoogleTagManager } from '@/components/google-tag-manager';
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-background">
-      <GoogleTagManager gtmId="GTM-M4V26FT4" />
+      <Suspense>
+        <GoogleTagManager gtmId="GTM-M4V26FT4" />
+      </Suspense>
       <Header />
       <main className="flex-1">
         <HeroSection />
